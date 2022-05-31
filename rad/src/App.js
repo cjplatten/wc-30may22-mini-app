0ebf2d385591398853b2f28fd0569d750a991b18
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-05-31 15:36:57 +0100

    initial app setup

---
+++ rad/src/App.js
@@ -0,0 +1,18 @@
+import skateboarder from './Images/skater-ghost.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1 className='App-header-h1'>✨😎stay radical😎✨</h1>
+        <img src={skateboarder} className="App-logo" alt="skateboarding ghost" />
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
