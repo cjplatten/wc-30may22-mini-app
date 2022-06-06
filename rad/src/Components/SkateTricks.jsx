1241aa9ed0c6c0157a86edcbf415f4a2a310cb9e
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-06-06 12:57:36 +0100

    add to SkateTricks

--- rad/src/Components/SkateTricks.jsx
+++ rad/src/Components/SkateTricks.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import skateboarder from "../Images/skater-ghost.png";
 import ramp from "../Images/small-skate-ramp-pbr-textures-01.jpeg";
 
-const SkateTricks = () => {
+const SkateTricks = (props) => {
+  const { setAppClass } = props;
+
   const [skateboarderClass, setSkateboarderClass] =
     useState("skateboarder-ready");
 
@@ -17,42 +19,47 @@ const SkateTricks = () => {
     setSkateboarderClass("skateboarder-faster");
   }
   function spinEvenFaster() {
-    setSkateboarderClass("skateboarder-even-faster")
+    setSkateboarderClass("skateboarder-even-faster");
   }
   function spinOhNoFaster() {
-    setSkateboarderClass("skateboarder-oh-no-faster")
+    setSkateboarderClass("skateboarder-oh-no-faster");
   }
-//   function whatHaveYouDone() {
-    
-//   }
-
-
+  function whatHaveYouDone() {
+    setAppClass("app-what-have-you-done");
+  }
 
   let buttons;
   if (skateboarderClass === "skateboarder-ready") {
     buttons = <button onClick={doAFlip}>Do a flip</button>;
   } else if (skateboarderClass === "skateboarder-flip") {
-   buttons = <>
-      <button onClick={pleaseStopFlipping}>Please Stop</button>
-      <button onClick={spinFaster}>SPIN FASTER</button>
-    </>;
+    buttons = (
+      <>
+        <button onClick={pleaseStopFlipping}>Please Stop</button>
+        <button onClick={spinFaster}>SPIN FASTER</button>
+      </>
+    );
   } else if (skateboarderClass === "skateboarder-faster") {
-    buttons =  <>
-      <button onClick={pleaseStopFlipping}>Please Stop</button>
-      <button onClick={spinEvenFaster}>EVEN FASTER</button>
-    </>;
+    buttons = (
+      <>
+        <button onClick={pleaseStopFlipping}>Please Stop</button>
+        <button onClick={spinEvenFaster}>EVEN FASTER</button>
+      </>
+    );
   } else if (skateboarderClass === "skateboarder-even-faster") {
-    buttons =  <>
-      <button onClick={pleaseStopFlipping}>Please Stop</button>
-      <button onClick={spinOhNoFaster}>Oh no</button>
-    </>;
+    buttons = (
+      <>
+        <button onClick={pleaseStopFlipping}>Please Stop</button>
+        <button onClick={spinOhNoFaster}>Oh no</button>
+      </>
+    );
   } else if (skateboarderClass === "skateboarder-oh-no-faster") {
-    buttons =  <>
-      <button onClick={pleaseStopFlipping}>Please Stop</button>
-      {/* <button onClick={whatHaveYouDone}>What have you done</button> */}
-    </>;
+    buttons = (
+      <>
+        <button onClick={pleaseStopFlipping}>Please Stop</button>
+        <button onClick={whatHaveYouDone}>What have you done</button>
+      </>
+    );
   }
-
 
   return (
     <div className="skate-tricks">
