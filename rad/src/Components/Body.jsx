591cca71aef97bf375ea6095859895e1b81b7d49
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-06-06 10:30:32 +0100

    add SkateTricks + animations

--- rad/src/Components/Body.jsx
+++ rad/src/Components/Body.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import skateboarder from '../Images/skater-ghost.png';
-import hellaRadTrees from '../Images/hella-radical-trees.webp';
-import goodVibes from '../Images/good-vibes.gif';
-import hellaRadical from '../Images/hella-radical.gif';
-import glitterText from '../Images/glitter-text.gif';
-import woah from '../Images/woah.webp';
-
+import hellaRadTrees from "../Images/hella-radical-trees.webp";
+import goodVibes from "../Images/good-vibes.gif";
+import hellaRadical from "../Images/hella-radical.gif";
+import glitterText from "../Images/glitter-text.gif";
+import woah from "../Images/woah.webp";
+import SkateTricks from "./SkateTricks";
 
 const Body = () => {
   return (
     <main className="Body">
-      <img src={hellaRadTrees} className="hella-rad-trees" alt="hella radical" />
-      <img src={skateboarder} className="skateboarder" alt="skateboarding ghost" />
+      <img
+        src={hellaRadTrees}
+        className="hella-rad-trees"
+        alt="hella radical"
+      />
       <img src={goodVibes} className="good-vibes" alt="good vibes" />
+      <SkateTricks />
+      <img src={woah} className="woah" alt="woah" />
       <img src={hellaRadical} className="hella-radical" alt="hella radical" />
-      <img src={glitterText} className="Body" alt="skateboarding ghost" />
-      <img src={woah} className="Body" alt="skateboarding ghost" />
+      <img src={glitterText} className="Body" alt="glitter text" />
     </main>
   );
 };
---
+++ rad/src/Components/SkateTricks.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import skateboarder from "../Images/skater-ghost.png";
+import ramp from "../Images/small-skate-ramp-pbr-textures-01.jpeg";
+
+const SkateTricks = () => {
+  const [skateboarderClass, setSkateboarderClass] =
+    useState("skateboarder-ready");
+
+  function doAFlip() {
+    setSkateboarderClass("skateboarder-flip");
+  }
+
+  function pleaseStopFlipping() {
+    setSkateboarderClass("skateboarder-ready");
+  }
+  function spinFaster() {
+    setSkateboarderClass("skateboarder-faster");
+  }
+  function spinEvenFaster() {
+    setSkateboarderClass("skateboarder-even-faster")
+  }
+  function spinOhNoFaster() {
+    setSkateboarderClass("skateboarder-oh-no-faster")
+  }
+//   function whatHaveYouDone() {
+    
+//   }
+
+
+
+  let buttons;
+  if (skateboarderClass === "skateboarder-ready") {
+    buttons = <button onClick={doAFlip}>Do a flip</button>;
+  } else if (skateboarderClass === "skateboarder-flip") {
+   buttons = <>
+      <button onClick={pleaseStopFlipping}>Please Stop</button>
+      <button onClick={spinFaster}>SPIN FASTER</button>
+    </>;
+  } else if (skateboarderClass === "skateboarder-faster") {
+    buttons =  <>
+      <button onClick={pleaseStopFlipping}>Please Stop</button>
+      <button onClick={spinEvenFaster}>EVEN FASTER</button>
+    </>;
+  } else if (skateboarderClass === "skateboarder-even-faster") {
+    buttons =  <>
+      <button onClick={pleaseStopFlipping}>Please Stop</button>
+      <button onClick={spinOhNoFaster}>Oh no</button>
+    </>;
+  } else if (skateboarderClass === "skateboarder-oh-no-faster") {
+    buttons =  <>
+      <button onClick={pleaseStopFlipping}>Please Stop</button>
+      {/* <button onClick={whatHaveYouDone}>What have you done</button> */}
+    </>;
+  }
+
+
+  return (
+    <div className="skate-tricks">
+      <img
+        src={skateboarder}
+        className={`skateboarder ${skateboarderClass}`}
+        alt="skateboarding ghost"
+      />
+      <img src={ramp} className="ramp" alt="skate ramp" />
+      {buttons}
+    </div>
+  );
+};
+
+export default SkateTricks;
